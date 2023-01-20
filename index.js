3a8795216d4bc4c1f4473995d6093310f2fa77b3
Mohammad Jahid <mohammadjahid0007@gmail.com>
2023-01-21 00:12:37 +0600

    c

--- index.js
+++ index.js
@@ -7,13 +7,17 @@ const app = express();
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const port = process.env.PORT || 5000;
 
+const corsOptions = {
+  origin: "https://doctors--portal-6b422.web.app",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 //middleware
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-});
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+// });
 
 // const http = require("http");
 // const server = http.createServer((req, res) => {
